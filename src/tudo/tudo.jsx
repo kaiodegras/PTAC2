93201637cba213d01c87e0a4cb66122827228fca
kaiodegras <kaioa9851@gmail.com>
2023-08-25 17:40:20 -0400

    ptac
    atividade

--- src/tudo/tudo.jsx
+++ src/tudo/tudo.jsx
@@ -1,9 +1,42 @@
+import { useState } from "react";
 import {Link} from "react-rounter-dom"
+
+
 export default function tudo (){
+    const [atividade, setAtividade] = useState("");
+    const [lista, setLista] = useState([]);
+
+    const [id, setId] = useState(1)
+
+
+    const salvar = (e) => {
+        e.preventDefault();
+        setLista([...lista,{
+            atividade: atividade, id:id
+        }])
+
+        setId(1 + 1)
+        console.log(lista)
+    }
+
     return(
         <div>
-            <h1>Home</h1>
             <Link to="/home">home</Link>
-        </div>
+            <h1>Lista de Atividades</h1>
+
+         <form onSubmit={salvar}>
+           <input type="text"
+             oneChange={(e) =>{setAtividade(e.target.value)}}/>
+             <button>ADD</button>
+         </form>
+         
+    {lista.map((ativ)=>
+      <div>
+        <p>{ativ.atividade}</p>
+      </div>
+    )}
+            
+            </div>
     )
+
 }
